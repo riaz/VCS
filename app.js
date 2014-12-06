d07e933a0fcc71448378692720aca957974b5443
harshdand <harshdand2@gmail.com>
2014-12-06 09:01:12 +0000

    Basic App Structure

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('vcs', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('vcs').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('vcs').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
