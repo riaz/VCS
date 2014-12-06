d85b6d67e7001b3ad2950118a0dfaf6c11ef221e
harshdand <harshdand2@gmail.com>
2014-12-06 18:20:48 +0000

    Update dnd

--- vsmain/partial/home/home.js
+++ vsmain/partial/home/home.js
@@ -1,5 +1,26 @@
 angular.module('vcs').controller('HomeCtrl',function($scope){
     $scope.init = function(){
-        $('#sr-5').dragdrop({ makeClone: true ,dragClass:'scenario-block-drag'});
+        $('.scenario-block').draggable({
+            helper: 'clone',
+            zIndex: 100,
+            start: function( event, ui ) {
+                $('.ui-draggable-dragging .scenario-details').addClass('ui-drag');
+            },
+            stop: function( event, ui ) {
+      
+            }
+        });
+
+        $('#map-container').droppable({
+            accept: '.scenario-block',
+            hoverClass: 'map-container-drop',
+            activeClass: 'map-container-droppable',
+            activate: function( event, ui ) {
+                $('#map-container').parent('.panel').css('border','0');
+            },
+            deactivate: function( event, ui ) {
+                $('#map-container').parent('.panel').css('border','2px solid #ecf0f1');
+            }
+        });
     }
 });
