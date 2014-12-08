b6d8e619be15ae7bb1d6960b552f755df3a8b23f
harshdand <harshdand2@gmail.com>
2014-12-08 07:21:31 +0000

    Added Simulation

--- vsmain/partial/home/home.js
+++ vsmain/partial/home/home.js
@@ -2,7 +2,7 @@
 /*global google:false */
 /*jshint -W030 */
 angular.module('vcs').controller('HomeCtrl',['$scope', 'mapconfig', '$firebase','$timeout','firebaseurl','carimages',function($scope, mapconfig, $firebase,$timeout,firebaseurl,carimages){
-    
+    $scope.speed = 10000;
     $scope.current_scenario = 'scenario1';
     $scope.ready = false;
     $scope.markers = [];
@@ -75,16 +75,95 @@ angular.module('vcs').controller('HomeCtrl',['$scope', 'mapconfig', '$firebase',
                 $('#map-holder').closest('.panel').css('border','2px solid #ecf0f1');
             },
             drop: function(event, ui){
-                $scope.current_scenario = ui.draggable.attr('data-scenario');
+                $scope.$apply(function(){
+                    $scope.current_scenario = ui.draggable.attr('data-scenario');
+                });
+                
                 // $scope.current_scenario_data = $scope.scenarios_data[i];
                 $scope.getScenarioData();
                 $scope.plotMarkers();
             }
         });
-    }
+    },
     
     $scope.play = function(){
-          
+         window.cars = [];
+         for(var i in $scope.markers){
+             if($scope.markers[i].startTime !== 0){
+                 $timeout(function(){
+                     if($scope.markers[i]){
+                        $scope.markers[i].setMap($scope.google_map);
+                        $scope.circles[i].setMap($scope.google_map);
+                     }
+                 },Math.floor($scope.markers[i].startTime));
+             }
+         }
+         
+         $scope.playCars();
+    },
+    
+    $scope.playCars = function(){
+        debugger;
+        // for(var i in $scope.markers){
+            $scope.timers[0] = setInterval(function () {
+            var directions = $scope.markers[0].directions;
+            if($scope.markers[0].currentFrame < $scope.markers[0].directions.length){
+                $scope.markers[0].setPosition($scope.markers[0].directions[$scope.markers[0].currentFrame]);
+                $scope.circles[0].setCenter($scope.markers[0].directions[$scope.markers[0].currentFrame]);
+                $scope.markers[0].currentFrame++;
+                $scope.google_map.panTo($scope.markers[0].directions[$scope.markers[0].currentFrame]);
+                console.log($scope.markers[1].currentFrame);
+                if(directions[$scope.markers[0].currentFrame].turn == 'left'){
+                    $scope.markers[0].setIcon(carimages.yellow_l_90);
+                    
+                    var circleOptions = {
+                    strokeColor: $scope.colors.red,
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: $scope.colors.red,
+                    fillOpacity: 0.35,
+                    center: directions[$scope.markers[0].currentFrame],
+                    radius: 20
+                    };
+                    
+                    $scope.circles[0].setMap(null);
+                    // $scope.circles[1].setMap(null);
+
+                    var circleOptionsNew = {
+                        strokeColor: $scope.colors.red,
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: $scope.colors.red,
+                        fillOpacity: 0.35,
+                        center: directions[$scope.markers[1].currentFrame],
+                        radius: 20
+                    };
+                    
+                    var circle = new google.maps.Circle(circleOptions);
+                    var circleNew = new google.maps.Circle(circleOptionsNew);
+                    $scope.circles[0] = circle;
+                    $scope.circles[1] = circleNew;
+                    circle.setMap($scope.google_map);
+                }
+                
+                if(directions[$scope.markers[0].currentFrame].turn == 'up'){
+                    $scope.markers[0].setIcon(carimages.yellow);
+                    
+                }
+            }
+            }, Math.floor($scope.speed/$scope.markers[0].speed));  
+            
+            $timeout(function(){
+                $scope.timers[1] = setInterval(function () {
+                var directions = $scope.markers[1].directions;
+                if($scope.markers[1].currentFrame < $scope.markers[1].directions.length){
+                    $scope.markers[1].setPosition($scope.markers[1].directions[$scope.markers[1].currentFrame]);
+                    $scope.circles[1].setCenter($scope.markers[1].directions[$scope.markers[1].currentFrame]);
+                    $scope.markers[1].currentFrame++;
+                }
+                }, Math.floor($scope.speed/$scope.markers[1].speed));  
+            // }
+            },Math.floor($scope.markers[1].startTime))
     },
     
     $scope.selectCar = function(direction,car){
@@ -125,18 +204,21 @@ angular.module('vcs').controller('HomeCtrl',['$scope', 'mapconfig', '$firebase',
     $scope.plotMarkers = function(){
         for(var i in $scope.current_scenario_data.cars){
             var icon = carimages.gray;
+            var marker;
+            var circle;
             var position = $scope.current_scenario_data.cars[i].directions[0];
             
             icon = $scope.selectCar($scope.current_scenario_data.cars[i].moving,$scope.current_scenario_data.cars[i].id);
 
-            if($scope.current_scenario_data.cars[i].startTime < 1000){ 
-               
-            }  
-
-            var marker = new google.maps.Marker({
+            marker = new google.maps.Marker({
                 animation: google.maps.Animation.DROP,
                 position: position,
-                icon: icon
+                icon: icon,
+                startTime: $scope.current_scenario_data.cars[i].startTime,
+                speed: $scope.current_scenario_data.cars[i].speed,
+                directions: $scope.current_scenario_data.cars[i].directions,
+                currentFrame: 0,
+                car:$scope.current_scenario_data.cars[i].id
             });
             
             $scope.markers.push(marker);
@@ -151,20 +233,25 @@ angular.module('vcs').controller('HomeCtrl',['$scope', 'mapconfig', '$firebase',
                 radius: 20
             };
 
-            var circle = new google.maps.Circle(circleOptions);
+            circle = new google.maps.Circle(circleOptions);
             $scope.circles.push(circle);
     
             if($scope.current_scenario_data.cars[i].startTime < 1000){ 
                 marker.setMap($scope.google_map);
                 circle.setMap($scope.google_map);
-            }  
-                
+            }          
         }
-    
     },
     
     $scope.stop = function(){
-        
+        $scope.current_scenario_data ={};
+        $scope.google_map= new google.maps.Map(document.getElementById('map-holder'),
+                    mapconfig.mapOptions);
+        $scope.current_scenario  = '';
+        clearInterval($scope.timers[0]);
+        clearInterval($scope.timers[1]);
+        $scope.markers = [];
+        $scope.circles = [];
     },
     
     $scope.getScenarioData = function(){
